8448d8fdc6acc57c02b918cb7faef1323a5be4d1
William Nafack <williamnafack@outlook.com>
2019-03-20 21:13:49 +0000

    HERE IS THE FIRST PART YOU CAN ADD IT TO YOUR CODE TO DO THE CHANGES TO PART 2

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -1,19 +1,94 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.unmodifiableCollection;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Collections.unmodifiableSet;
+import static java.util.Objects.requireNonNull;
+import static uk.ac.bris.cs.scotlandyard.model.Colour.BLACK;
+import static uk.ac.bris.cs.scotlandyard.model.Ticket.DOUBLE;
+import static uk.ac.bris.cs.scotlandyard.model.Ticket.SECRET;
 
+import java.util.*;
+import java.util.function.Consumer;
+import uk.ac.bris.cs.gamekit.graph.Edge;
 import uk.ac.bris.cs.gamekit.graph.Graph;
+import uk.ac.bris.cs.gamekit.graph.ImmutableGraph;
+
 
 // TODO implement all methods and pass all tests
 public class ScotlandYardModel implements ScotlandYardGame {
 
+	List<Boolean> rounds;
+	Graph<Integer, Transport> graph;
+
 	public ScotlandYardModel(List<Boolean> rounds, Graph<Integer, Transport> graph,
 			PlayerConfiguration mrX, PlayerConfiguration firstDetective,
 			PlayerConfiguration... restOfTheDetectives) {
-		// TODO
+
+		this.rounds = requireNonNull(rounds);
+		if (rounds.isEmpty()) {
+			throw new IllegalArgumentException("Empty rounds");
+		}
+
+		this.graph = requireNonNull(graph);
+		if (graph.isEmpty()) {
+			throw new IllegalArgumentException("Empty graph");
+		}
+
+
+		if (mrX.colour != BLACK) { // or mr.colour.isDetective()
+			throw new IllegalArgumentException("MrX should be Black");
+		}
+
+		List<PlayerConfiguration> configurations = new ArrayList<>();
+		configurations.add(0, mrX);
+		configurations.add(1,firstDetective);
+		for (PlayerConfiguration configuration : restOfTheDetectives) {
+			configurations.add(requireNonNull(configuration));
+
+
+		}
+
+		Set<Integer> set = new HashSet<>();
+		for (PlayerConfiguration configuration : configurations) {
+			if (set.contains(configuration.location)) {
+				throw new IllegalArgumentException("Duplicate location");
+			}
+			set.add(configuration.location);
+		}
+
+		Set<Colour> set1 = new HashSet<>();
+		for (PlayerConfiguration configuration : configurations) {
+			if (set1.contains(configuration.colour))
+				throw new IllegalArgumentException("Duplicate colour");
+			set1.add(configuration.colour);
+		}
+		
+		for(PlayerConfiguration configuration : configurations) {
+			if (configuration.tickets.get(Ticket.SECRET) == null)
+				throw new IllegalArgumentException("ALL TYPE TICKET MUST EXIST");
+			if (configuration.tickets.get(Ticket.DOUBLE) == null)
+				throw new IllegalArgumentException("ALL TYPE TICKET MUST EXIST");
+			if (configuration.tickets.get(Ticket.BUS) == null)
+				throw new IllegalArgumentException("ALL TYPE TICKET MUST EXIST");
+			if (configuration.tickets.get(Ticket.TAXI) == null)
+				throw new IllegalArgumentException("ALL TYPE TICKET MUST EXIST");
+			if (configuration.tickets.get(Ticket.UNDERGROUND) == null)
+				throw new IllegalArgumentException("ALL TYPE TICKET MUST EXIST");
+			if ((configuration.tickets.get(Ticket.SECRET) != 0) & configuration.colour != BLACK)
+				throw new IllegalArgumentException("Detectives Do not have SECRET TICKETS ");
+			if ((configuration.tickets.get(Ticket.DOUBLE) != 0) & configuration.colour != BLACK)
+				throw new IllegalArgumentException("Detectives Do not have DOUBLE TICKETS");
+		}
+
+		List<ScotlandYardPlayer> players = new ArrayList<>();
+		players.add(0,new ScotlandYardPlayer(mrX.player,mrX.colour,mrX.location,mrX.tickets));
+		players.add(1,new ScotlandYardPlayer(firstDetective.player,firstDetective.colour,firstDetective.location,firstDetective.tickets));
+		for(PlayerConfiguration configuration : restOfTheDetectives)
+			players.add( new ScotlandYardPlayer(configuration.player,configuration.colour,configuration.location,configuration.tickets));
 	}
 
 	@Override
