4c210296dc34adf603d49d0bf19a142aa76f37df
William Nafack <williamnafack@outlook.com>
2019-04-29 19:20:57 +0100

    FINAL VERSION MODEL

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/NotifySpectators.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/NotifySpectators.java
@@ -1,10 +1,5 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
-import uk.ac.bris.cs.scotlandyard.model.Colour;
-import uk.ac.bris.cs.scotlandyard.model.Move;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYardView;
-import uk.ac.bris.cs.scotlandyard.model.Spectator;
-
 import java.util.*;
 
 import static java.util.Objects.requireNonNull;
@@ -13,8 +8,7 @@ public class NotifySpectators {
 
     private ArrayList <Spectator> spectators = new ArrayList <> ();
 
-    public Collection<Spectator> getSpectators() {
-
+    Collection<Spectator> getSpectators() {
         return Collections.unmodifiableCollection(spectators);
     }
 
@@ -27,7 +21,7 @@ public class NotifySpectators {
         }
     }
 
-    public void unregisterSpectator(Spectator spectator) {
+    void unregisterSpectator(Spectator spectator) {
         if (spectators.contains(requireNonNull(spectator))){
             spectators.remove(spectator);
         }
@@ -35,29 +29,29 @@ public class NotifySpectators {
             throw new IllegalArgumentException("No spectator is registered");
         }
     }
-
-    public void GameisOver(ScotlandYardView view, Set<Colour> win){
+    /** notify  game is over **/
+    void gameIsOver(ScotlandYardView view, Set<Colour> win){
         for (Spectator spectator : spectators){
             spectator.onGameOver(view, win);
         }
     }
 
-    //A helper method to notify all the spectators that a round has started
-    public void RoundhasStarted(ScotlandYardView view, int currentRound){
+    /** notify  round has started **/
+    void roundHasStarted(ScotlandYardView view, int currentRound){
         for (Spectator spectator : spectators){
             spectator.onRoundStarted(view, currentRound);
         }
     }
 
-    //A helper method to notify all the spectators that a move has been made
-    public void MoveisMade(ScotlandYardView view, Move move){
+    /** notify  move is made**/
+    void moveIsMade(ScotlandYardView view, Move move){
         for (Spectator spectator : spectators){
             spectator.onMoveMade(view, move);
         }
     }
 
-    //A helper method to notify all the spectators that a round has ended
-    public void RotationisComplete(ScotlandYardView view){
+    /** notify rotation is complete **/
+    void rotationIsComplete(ScotlandYardView view){
         for (Spectator spectator : spectators){
             spectator.onRotationComplete(view);
         }
